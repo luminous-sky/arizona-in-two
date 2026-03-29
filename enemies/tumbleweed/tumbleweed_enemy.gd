bdd38cbe2ad76be76af491b7190d0dbbe3e9c878
Justbetter14 <145606090+Justbetter14@users.noreply.github.com>
2026-03-29 13:18:28 -0400

    Added tumbleweed script to tumbleweed
    Changed the location of script from being in the ether to the correct folder

--- enemies/tumbleweed/tumbleweed_enemy.gd
+++ enemies/tumbleweed/tumbleweed_enemy.gd
@@ -1,48 +1,45 @@
 extends CharacterBody2D
 
-@export var speed: float = 10.0
+@export var speed: float = 300.0
 
-@export var attackDmg: float = 10.0
+@export var attackDmg: float = 15.0
 
-var minDist: float = 15.0
-
-var maxDist: float = 25.0
-
-var targetPosition := position
-
-var targetAngle: Vector2 = Vector2(0, 0)
+var targetAngle: Vector2 = [Vector2(1,0), Vector2(0, 1), Vector2(-1, 0), Vector2(0, -1)][randi_range(0,3)]
+var canMove: bool = false
+var shouldMove: bool = true
 
 var player: CharacterBody2D = null
+var player_LastKnownPosition: Vector2 = Vector2.ZERO
 
 var spread: float = PI/6
 
 var mode: String = "Idle"
 
 @onready var timer: Timer = $"Movement Timer"
+@onready var delay: Timer = $"Movement Delay"
 @onready var attackTimer: Timer = $"Attack Delay"
-
-func _ready() -> void:
-	pass # Replace with function body.
-
 
 func _physics_process(delta: float) -> void:
 	if mode == "Idle":
-		if position == targetPosition:
-			velocity = Vector2(0,0)
-			timer.start()
+		if not attackTimer.is_stopped():
+			return 
+			
+		if canMove:
+			move_and_slide()
+			return
 		
-		if timer.is_stopped():
-			velocity = targetAngle * speed
+		if not shouldMove:
+			return 
+			
+		velocity = speed * targetAngle * delta
+		targetAngle = [Vector2.RIGHT, Vector2.DOWN, Vector2.LEFT, Vector2.UP][randi_range(0,3)]
+		timer.wait_time = randf_range(0.1, 5.0)
+		timer.start()
+		canMove = true
+		shouldMove = false
 		
-		move_and_slide()
 	elif mode == "Attack":
 		attack()
-
-func movement():
-	var direction: Vector2 = [Vector2(1,0), Vector2(0, 1), Vector2(-1, 0), Vector2(0, -1)][randi_range(0,3)]
-	var distance: float = randf_range(minDist, maxDist)
-	
-	targetPosition = position - (direction * distance)
 
 func attack():
 	if attackTimer.is_stopped():
@@ -54,24 +51,22 @@ func damaged_sequence():
 func suicide():
 	queue_free() # Death Animation
 
-
 func _on_attack_range_body_entered(body: Node2D) -> void:
 	if body is CharacterBody2D and body.is_in_group("Player"):
 		player = body
 		mode = "Attack"
-		timer.stop()
 
 func _on_attack_range_body_exited(body: Node2D) -> void:
 	if body is CharacterBody2D and body.is_in_group("Player"):
+		player_LastKnownPosition = player.position
 		player = null
 		mode = "Idle"
-		timer.start()
-
 
 func _on_movement_timer_timeout() -> void:
-	if position == targetPosition:
-		movement()
-
+	canMove = false
+	delay.wait_time = randf_range(0.1, 5.0)
+	delay.start()
+	
 
 func _on_attack_delay_timeout() -> void:
 	var bullet1 = load("res://enemies/tumbleweed/tumble_bullet.tscn").instantiate()
@@ -82,10 +77,19 @@ func _on_attack_delay_timeout() -> void:
 	bullet2.position = position
 	bullet3.position = position
 	
-	bullet1.direction = player.position - position
-	bullet2.direction = (player.position - position).rotated(spread)
-	bullet2.direction = (player.position - position).rotated(-spread)
+	if player == null:
+		bullet1.direction = (player_LastKnownPosition - position)
+		bullet2.direction = (player_LastKnownPosition - position).rotated(spread)
+		bullet3.direction = (player_LastKnownPosition - position).rotated(-spread)
+	else:
+		bullet1.direction = (player.position - position)
+		bullet2.direction = (player.position - position).rotated(spread)
+		bullet3.direction = (player.position - position).rotated(-spread)
 	
 	get_parent().add_child(bullet1)
 	get_parent().add_child(bullet2)
 	get_parent().add_child(bullet3)
+
+
+func _on_movement_delay_timeout() -> void:
+	shouldMove = true
